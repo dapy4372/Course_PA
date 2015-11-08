e2de278abd628fc7111ab55ab10ab2e9f1824921
hsiungchunyu <zoidsrz041@yahoo.com.tw>
2015-11-08 21:37:46 +0800

    filler.py_debug_v1

--- src/filler.py
+++ src/filler.py
@@ -32,19 +32,21 @@ def fillerCore(Set):
     currentName,a=namepick(Set[2][0])
     for i in range(len(Set[2])):
         count=count+1
+        name,b=namepick(Set[2][i])
+        if currentName!=name:
+            for j in range(maxLength-count+1):
+                fillSet[0].append(numpy.zeros(len(Set[0][0])))
+                fillSet[1].append(-1)
+                fillSet[2].append(currentName+"_"+str(j+count+1))
+                fillSet[3].append(-1)
+            count=1
+            currentName=name
+        
         fillSet[0].append(Set[0][i])
         fillSet[1].append(Set[1][i])
         fillSet[2].append(Set[2][i])
         fillSet[3].append(1)
-        name,b=namepick(Set[2][i])
-        if currentName!=name:
-            for j in range(maxLength-count):
-                fillSet[0].append(numpy.zeros(len(Set[0][0])))
-                fillSet[1].append(-1)
-                fillSet[2].append(currentName+"_"+str(j+count))
-                fillSet[3].append(-1)
-            count=0
-            currentName=name
+
     for i in range((len(fillSet[0])/maxLength)):
         finalSet[0].append(fillSet[0][i*maxLength:(i+1)*maxLength-1])
         finalSet[1].append(fillSet[1][i*maxLength:(i+1)*maxLength-1])
@@ -54,6 +56,7 @@ def fillerCore(Set):
 
 def filler():
     dataSet=loadDataset("../pkl/fbank_69_dataset.pkl",3)
+    print dataSet[0][2][0:300]
     fill_trainSet=fillerCore(dataSet[0])
     fill_validSet=fillerCore(dataSet[1])
     fill_testSet =fillerCore(dataSet[2])
@@ -61,4 +64,4 @@ def filler():
 
 a,b,c=filler()
 
-print a[2][0]
+print a[1][0]
