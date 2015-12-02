3b444d6e89a32da76577d96ad1a73b8c7c1de843
hsiungchunyu <zoidsrz041@yahoo.com.tw>
2015-12-02 15:54:32 +0800

    HMM_with_training

--- src/HMM/HMM.py
+++ src/HMM/HMM.py
@@ -12,12 +12,12 @@ from HMM_core import HMM_core
 from utils import loadDataset
 import math
 
+filename="HMM_test_bear2"#output filename
 DEBUG = False
 FER_PER_SENT = True
 PAUSE = False
 OUTPUT_DETAIL = False
 THRESHOLD = 0.27
-
 #parameterFilename = sys.argv[1]
 #np.set_printoptions(threshold=np.nan) # for print np array
 
@@ -26,41 +26,47 @@ def trainHMM(datasets):
     trainSetX, trainSetY, trainSetName = rnnUtils.makeDataSentence(datasets[0])
     validSetX, validSetY, validSetName = rnnUtils.makeDataSentence(datasets[1])
     testSetX,  testSetY,  testSetName  = rnnUtils.makeDataSentence(datasets[2])
-
+    '''
+    SetX=[]
+    SetY=[]
+    
+    for i in xrange(100):
+        SetX.append(trainSetX[i])
+        SetY.append(trainSetY[i])
+    trainSetX=SetX
+    trainSetY=SetY
+    VSetX=[]
+    VSetY=[]
+    for i in xrange(20):
+        VSetX.append(validSetX[i])
+        VSetY.append(validSetY[i])
+    validSetX=VSetX
+    validSetY=VSetY
+    '''
     '''
     if P.cutSentSize > 0:
         trainSetX, trainSetY, trainSetName = rnnUtils.cutSentence([trainSetX, trainSetY, trainSetName], P.cutSentSize)
         validSetX, validSetY, validSetName = rnnUtils.cutSentence([validSetX, validSetY, validSetName], P.cutSentSize)
-
     '''
+    
     ###############
     # BUILD MODEL #
     ###############
     
     print '... building the model'
-    idx = T.iscalar('i')
-    x = T.matrix()
-    y = T.ivector()
     stateNum=len(trainSetX[0][0])#total state/phone
     pi=numpy.zeros(stateNum,dtype=theano.config.floatX)
     for i in xrange(len(trainSetY)):#count how many start
         pi[trainSetY[i][0]]+=1
+
     for i in xrange(stateNum):#initial prob for 1/totalStateNum
         pi[i]=pi[i]/float(len(trainSetY))
     print pi
-    A=numpy.zeros((stateNum,stateNum),dtype=theano.config.floatX)
-    totalNum=[]
+    A,countA=tran_prob_matrix_A(trainSetY,stateNum)
+    '''
     for i in xrange(stateNum):
-        totalNum.append(0)
-    for i in xrange(len(trainSetY)):#num of sentence
-        for j in xrange(len(trainSetY[i])-1):#num of element transition in each sentence
-            A[trainSetY[i][j],trainSetY[i][j+1]]+=1#count how many times for each transitions
-            totalNum[int(trainSetY[i][j])]+=1 
-    for i in xrange(stateNum):# for each element/totalNum of observed outputs
-        for j in xrange(stateNum):
-            A[j,i]=float(A[j,i])/float(totalNum[j]) 
-    for i in xrange(stateNum):
-        print A[i,:]
+       print countA[i,:]
+    '''
     #best parameters
     best_A=A
     best_pi=pi
@@ -78,113 +84,224 @@ def trainHMM(datasets):
     
  
     # For create a new model
-    classifier=HMM_core(input=x,pi=pi,A=A)
-    '''
-    myOutputs=classifier.P_O_Q
-    dA=T.grad(myOutputs,classifier.A)
-    dpi=T.grad(myOutputs,classifier.pi)
-                 
-    trainModel=theano.function(inputs=[x],outputs=myOutputs,updates=myUpDates([classifier.A,classifier.pi],[dA,dpi])) 
-    times=0#training times
-    '''
+    classifier=HMM_core(pi=pi,A=A,countA=countA)
     errorRate=1
+    #training...
     print "... start training"
-    #validSet test
-    '''
-    totalElement=0
+    trainTimes=0
+    #initial error rate
+    #validSetX=B_process(validSetX,trainSetY)
+    
+    accumulateWrong,totalElement=validMeasure(classifier,validSetX,validSetY)
+    errorRate=float(accumulateWrong)/float(totalElement)
+    print "errorRate:",errorRate
+    write_trans_prob(A,"./prob_A/A_0.1:"+str(errorRate))
+     
+    
+    while(trainTimes<5):
+        trainTimes+=1
+        ViterbiLearning(classifier,trainSetX,trainSetY)
+        #validSetX=B_process(validSetX,validSetY)
+        
+        #EMLearning(classifier,trainSetX)
+
+        accumulateWrong,totalElement=validMeasure(classifier,validSetX,validSetY)
+        print "totalWrong:",accumulateWrong
+        print "totalNum:",totalElement
+        print "train error rate:",float(accumulateWrong)/float(totalElement)
+        if (float(accumulateWrong)/float(totalElement))<errorRate:
+            errorRate=float(accumulateWrong)/float(totalElement)
+            best_A=numpy.array(classifier.A,dtype=theano.config.floatX)
+            best_pi=classifier.pi
+            
+            write_trans_prob(A,"./prob_A/A_with_error:"+str(errorRate))
+        print "current_error:",errorRate
+    #bestModel to decode testSet
+    
+    #testSetX=B_process(testSetX,trainSetY)
+    bestClass=HMM_core(pi=best_pi,A=best_A,countA=countA)
+    outputTest(bestClass,testSetX,testSetName,filename)
+
+def get_result(fileName):
+    A=numpy.array(load_trans_prob(fileName),dtype=theano.config.floatX)
+    print A
+   
+def EMLearning(classifier,SetX):
+    for i in xrange(len(SetX)):
+        B_matrix=numpy.array(SetX[i],dtype=theano.config.floatX)
+        B_matrix=B_matrix.T
+ 
+        classifier.EM_learn(B_matrix)
+
+
+def ViterbiLearning(classifier,SetX,SetY):
+    resultSet=[]
+    for i in xrange(len(SetY)):
+        resultSet.append([])
+
+    for i in xrange(len(SetY)):
+        B_matrix=numpy.array(SetX[i],dtype=theano.config.floatX)
+        B_matrix=B_matrix.T
+        resultSet[i]=classifier.Viterbi(B_matrix)
+        #print i
+    stateNum=len(SetX[0][0])
+    trans_prob_revise(classifier,SetY,resultSet,stateNum)
+    for i in xrange(stateNum):
+        print classifier.A[i,:]
+
+def trans_prob_revise(classifier,SetY,resultSet,stateNum):
+    #calc original tran_prob_matrix
+    totalNum=[]
+    for i in xrange(stateNum):
+        totalNum.append(0)
+    for i in xrange(stateNum):#num of sentence
+        for j in xrange(stateNum):#num of element transition in each sentence
+            totalNum[i]+=classifier.countA[i][j]
+    #start to revise
+    for i in xrange(len(SetY)):
+        for j in xrange(len(SetY[i])-1):
+            seqNum=j+1# 1st wrong belong to initial prob
+            if (resultSet[i][seqNum]!=SetY[i][seqNum])&(classifier.countA[int(resultSet[i][seqNum-1]),int(resultSet[i][seqNum])]>0):
+                classifier.countA[int(resultSet[i][seqNum-1]),int(resultSet[i][seqNum])]-=0.1
+                classifier.countA[int(SetY[i][seqNum-1]),int(SetY[i][seqNum-1])]+=-0.1
+                totalNum[int(resultSet[i][seqNum-1])]-=0.1
+                totalNum[int(SetY[i][seqNum-1])]+=0.1
+    for i in xrange(stateNum):# for each element/totalNum of observed outputs
+        for j in xrange(stateNum):
+            classifier.A[j,i]=float(classifier.countA[j,i])/float(totalNum[j])
+
+def B_process(SetX,SetY):#P(O|S)=P[S|O](from DNN)*P[O](neglect)/P[S](what we do now:for /P[S])
+    count_y=[]
+    for i in xrange(len(SetX[0][0])):#state Num
+        count_y.append(0)
+    totalNum=0
+    for i in xrange(len(SetY)):
+        for j in xrange(len(SetY[i])):
+            count_y[int(SetY[i][j])]+=1#count[i]=num of state i
+            totalNum+=1
+    for i in xrange(len(count_y)):
+        count_y[i]=float(count_y[i])/float(totalNum)#count[i]/=totalNum->prob of state i
+        print "count[",i,"]=",count_y[i]
+    for i in xrange(len(SetX)):
+        for j in xrange(len(SetX[i])):
+            for k in xrange(len(SetX[i][j])):
+                SetX[i][j][k]=float(SetX[i][j][k])/float(count_y[k])
+    
+    return SetX
+
+def validMeasure(classifier,validSetX,validSetY):#given variable with validSet to measure error
     accumulateWrong=0
+    totalElement=0
+
     for i in xrange(len(validSetX)):
         B_matrix=numpy.array(validSetX[i],dtype=theano.config.floatX)
         B_matrix=B_matrix.T
-        print B_matrix.shape
+        #print B_matrix.shape
+
         y_result=classifier.Viterbi(B_matrix)
         y_ref=validSetY[i]
+        
         for j in xrange(len(y_result)):#calculate total wrong estimated element
             #print "(",y_result[j],",",y_ref[j],")"
             if y_result[j]!=y_ref[j]:
                 accumulateWrong=accumulateWrong+1
-        totalElement=totalElement+len(y_ref)#calculate total element    
-        print "wrongNum:",accumulateWrong  
-        print "totalNum:",totalElement  
-    print "current_error:",#if new errorRate<current errorRate: renew parameter
+        totalElement=totalElement+len(y_ref)#calculate total element
+        '''
+        acc=editDistance(y_ref,y_result)
+        accumulateWrong+=acc
+        totalElement+=len(y_ref)
+        '''
+        #print "wrongNum:",accumulateWrong
+        #print "totalNum:",totalElement
+    return accumulateWrong,totalElement
+def editDistance(y_ref,y_out):
+    r = []	
+    h = []
+    y_col=y_ref.shape
+    print y_col[0]
+    for i in xrange (y_col[0]):
+        r.append(y_ref[i])
+        h.append(y_out[i])
+
+    d = numpy.zeros((len(r)+1)*(len(h)+1), dtype=numpy.uint8)
+    d = d.reshape((len(r)+1, len(h)+1))
+    for i in range(len(r)+1):
+	for j in range(len(h)+1):
+            if i == 0:
+               	d[0][j] = j
+      	    elif j == 0:
+                d[i][0] = i
+
+    # computation
+    for i in range(1, len(r)+1):
+        for j in range(1, len(h)+1):
+            if r[i-1] == h[j-1]:
+                d[i][j] = d[i-1][j-1]
+            else:
+                substitution = d[i-1][j-1] + 1
+                insertion    = d[i][j-1] + 1
+                deletion     = d[i-1][j] + 1
+                d[i][j] = min(substitution, insertion, deletion)
+
+    #edit distance
+    print d[len(r)][len(h)]
+    return d[len(r)][len(h)]
+    #error rate
+    #print float(d[len(r)][len(h)])/float(len(r))
     
-    if (float(accumulateWrong)/float(totalElement))<errorRate:
-        errorRate=float(accumulateWrong)/float(totalElement)
-        best_A=classifier.A
-        best_pi=classifier.pi
-        print errorRate
+def tran_prob_matrix_A(SetY,stateNum):#given validSetY count probability transition matrix A
+    A=numpy.zeros((stateNum,stateNum),dtype=theano.config.floatX)
+    totalNum=[]
+    for i in xrange(stateNum):
+        totalNum.append(0)
+    for i in xrange(len(SetY)):#num of sentence
+        for j in xrange(len(SetY[i])-1):#num of element transition in each sentence
+            A[SetY[i][j],SetY[i][j+1]]+=1#count how many times for each transitions
+            totalNum[int(SetY[i][j])]+=1
+    countA=numpy.zeros((stateNum,stateNum),dtype=theano.config.floatX)
+    for i in xrange(stateNum):
+        for j in xrange(stateNum):
+            countA[i,j]=A[i,j]
+       
+    for i in xrange(stateNum):# for each element/totalNum of observed outputs
+        for j in xrange(stateNum):
+            A[j,i]=float(A[j,i])/float(totalNum[j])
     '''
-    #for i in xrange(len(trainSetY)):
+    for i in xrange(stateNum):
+            print A[i,:]
+            count=0
+            for j in xrange(stateNum):
+                count+=A[i,j]
+            print "count:",count
     '''
-    print dataSet[0][0][40],dataSet[0][1][40]
-    while (times<100) or (not((A==best_A) and (pi==best_pi))):#training
-        for i in xrange(len(trainSetY)):
-            B=numpy.array(B_list[i],dtype=theano.config.floatX)
-            print B
-            outputs=trainModel(B)
-            print ("outputs=%d"%(outputs))
-        #validSet test
-        time=time+1
-        totalElement=0
-        accumulateWrong=0
-        for i in xrange(len(ValidSetY)):
-            B=[]
-            for j in xrange(len(ValidSetX[i])):
-                B.append(ValidSetX[i][j]) #get_conditional_probability:use DNN
-            B_matrix=numpy.array(B,dtype=theano.config.floatX)
-            y_result=classifier.Viterbi(B_matrix) 
-            y_ref=ValidSetY[i]
-            for j in xrange(len(y_ref)):#calculate total wrong estimated element
-                if y_result[i]!=y_ref[i]:
-                    accumulateWrong=accumulateWrong+1
-            totalElement=totalElement+len(y_ref)#calculate total element
-        print "current_error:%d",#if new errorRate<current errorRate: renew parameter
+    return A,countA
 
-        if (float(accumulateWrong)/float(totalElement))<errorRate:
-            errorRate=float(accumulateWrong)/float(totalElement) 
-            best_A=classifier.A
-            best_pi=classifier.pi   
-    '''
-    outputTest(classifier,testSetX,testSetName)
 
-   
-def myUpDates(parameters,grads):
-    parameters_updates=[(p,update_function(p,g))for (p,g) in izip(parameters,grads)]
-    return parameters_updates
-def update_function(p,g):#algorithm:  http://www.robots.ox.ac.uk:5000/~vgg/rg/papers/hmm.pdf  page10
-    row,col=p.shape
-    updates=numpy.zeros((row,col),dtype=theano.config.floatX)
-    for i in xrange(col):#total setNum
-        denominator=0
-        for j in xrange(row):#total state
-            updates[j,i]=p*g
-            denominator=denominator+p*g
-        for j in xrange(row):
-            updates[j,i]=float(update[j,i])/float(denominator)
-    return updates
 
-'''
-def loadProb(fileName):
+def load_trans_prob(fileName):
     print "... loading conditional probability"
     f = open(fileName, 'rb')
     fileRead=f.read()
     fileSplit=fileRead.split('\n')# cut changing line to get each element
+    fileSplit.pop()
     fileProb=[]
     for i in xrange(len(fileSplit)):#initialize fileProb[] to fileProb[][] 
         fileProb.append([])
-    for i in xrange(len(fileSplit)):
+    for i in xrange(len(fileProb)):
         store=fileSplit[i].split(' ')#split by ' ' to get name and each number of probability
+        store.pop()
         for j in xrange(len(store)):#fill in fileProb[given element][j]
             fileProb[i].append(store[j])
     for i in xrange(len(fileSplit)):
-        for j in xrange(len(fileProb[i])-1):
-            x=fileProb[i][j+1].split('e')#j+1 because fileProb[i][0] is filename, ex:file 2.33432e-03
+        for j in xrange(len(fileProb[i])):
+            x=fileProb[i][j].split('e')#j+1 because fileProb[i][0] is filename, ex:file 2.33432e-03
             #change from character to float number
             if len(x)==1:
-                fileProb[i][j+1]=float(x[0])
+                fileProb[i][j]=float(x[0])
             else:
-                fileProb[i][j+1]=float(x[0])*10**float(x[1])
-    return fileProb
+                fileProb[i][j]=float(x[0])*10**float(x[1])
+    return fileProb#fileProb[i][j]=A[i,j]
+'''
 def get_conditional_prob(Name,File):#search each sequence by name
     returnArray=[]
     for i in xrange(len(File)):
@@ -194,13 +311,13 @@ def get_conditional_prob(Name,File):#search each sequence by name
             break
     return returnArray  
 '''           
-def outputTest(classifier,testSetX,testSetName):
+def outputTest(classifier,testSetX,testSetName,name):
     
-    fout = open('HMM_test_output', 'w')
+    fout = open(name, 'w')
     for i in xrange(len(testSetX)):
         B_matrix=numpy.array(testSetX[i],dtype=theano.config.floatX)
         B_matrix=B_matrix.T
-        print B_matrix.shape
+        #print B_matrix.shape
         y_result=classifier.Viterbi(B_matrix)
     
         for j in xrange(len(testSetX[i])):
@@ -211,7 +328,16 @@ def outputTest(classifier,testSetX,testSetName):
             fout.write("\n")
 
 
-
+def write_trans_prob(A,name):
+    stateNum,A_col=A.shape
+    fout=open(name,'w')
+    for i in xrange(stateNum):
+       for j in xrange(stateNum):
+           fout.write(str(A[i,j]))
+           fout.write(" ")
+       fout.write("\n")
+    
+    
 
 def dataCorrectRate(Set):
     correct=0
@@ -229,7 +355,9 @@ def dataCorrectRate(Set):
     print "correct rate:",float(correct)/float(totalNum)
     
 if __name__ == '__main__':
-
     dataSet=loadDataset("../../pkl/t26v31.pkl",3)
-    #dataCorrectRate(dataSet[0])
+    
+    #dataSet=loadDataset("/home/roylu/share/DNNResult/t23.7v29.95/t23.7v29.95.pkl",3)
+    dataCorrectRate(dataSet[0])
     trainHMM(dataSet)
+    #get_result("A_with_error:0.281822988462")
--- src/HMM/HMM_core.py
+++ src/HMM/HMM_core.py
@@ -4,78 +4,113 @@ import theano
 import math
 from utils import loadDataset,namepick
 
-'''
-def Viterbi(pi,A,B):#pi for initial distribution;A for transition matrix A(i,j):state i to state j; B for conditional probability:B[i,j]=P(jth Output|state i);O output observe sequence
-    A_row,A_col=A.shape
-    B_row,B_col=B.shape
-    path=[]
-    value=[]
-    for i in xrange(A_row):
-        path.append([])
-    #initial state 
-    for i in xrange(B_row):
-        v[i]=pi[i]*B[i,0]
-        path[i].append[i]
-    #main part
-    for i in xrange(B_col-1):
-    #i th output
-        seqNum=i-1
-        for j in xrange(B_row):
-            #to j th state
-            array=[]
-            for k in xrange(B_row):# k th last state
-                array.append(value[k]*A(k,j))
-            value[j],maxState=getMax(array)
-            path[j].append(maxState)
-    #find max path over, then trace back
-    maxValue,maxState=getMax(value)
-    y_sequence=[B_col]            
-    for i in xrange(B_col):
-        y_sequence[B_col-i]=path[maxState][B_col-i]
-        maxState=path[maxState][B_col-i]
-    return y_sequence    
-
-
-def getMax(array,maxState):
-    maximum=0
-    for i in xrange(len(array)):
-        if maximum<array[i]
-            maximum=array[i]
-    return maximum,maxState
-'''
 
 class HMM_core(object):
-    def __init__(self,input,pi,A):
+    def __init__(self,pi,A,countA):
         #self.pi=theano.shared(value=numpy.array(pi,dtype=theano.config.floatX),name='Pi',borrow=True)#initial distribution
         #self.A=theano.shared(value=numpy.array(A,dtype=theano.config.floatX),name='A',borrow=True)#transition matrix
         self.A=A
         self.pi=pi
-        self.P_O_Q=self.calc_P(input)
-    def calc_P(self,B):
-        #B_row,B_col=B.shape
-        B_row=48
-        B_col=10
+        self.countA=countA#A with counting number with every element(for Viterbi learning)
+    def calc_Log_apha(self,B):#apha[state][time]
+       apha=[]
+       B_row,B_col=B.shape
+       for i in xrange(B_row):
+           apha.append([]) 
 
-        apha_current=[]
-        apha_next=[]
-        for i in xrange(B_row):
-            apha_current.append([])
-            apha_next.append([])
-        for i in xrange(B_row):#initial
-            apha_current[i]=self.pi[i]*B[i,1]
-        for i in xrange(B_col-1):#for all observed output
-            for j in xrange(B_row):#for alpha[j state]
-                store_value=0
-                for k in xrange(B_row):#sigma apha[i]*aij  
-                    store_value=store_value+apha_current[k]*self.A[k,j]
-                apha_next[j]=store_value*B[j,i+1]
-            apha_current=apha_next
-        P=0
-        for i in xrange(B_row):#last sum up
-           P=P+apha_current[i] 
-        return P
+       for i in xrange(B_col):
+           for j in xrange(B_row):
+               if i==0:
+                   apha[j].append(self.getLog(self.pi[j])+self.getLog(B[j,0]))
+               else:
+                   p=-1*10**20
+                   for k in xrange(B_row):#apha[j][i]=sigma(k)[apha[k][i-1]*akj*B[j,i]
+                       p=self.addLog(p,(apha[k][i-1]+self.getLog(self.A[k,j]))) 
+                   apha[j].append(p+self.getLog(B[j,i]))
+       return apha
                 
+    def calc_Log_beta(self,B):#beta[state][time]
+        #initialize storage
+        beta=[]
+        B_row,B_col=B.shape
+        for i in xrange(B_row):
+            beta.append([])
+        for i in xrange(B_row):
+            for j in xrange(B_col):
+                beta[i].append(0)
+        #calculate
+        for i in xrange(B_col):#i:ith
+            seqNum=B_col-1-i
+            for j in xrange(B_row):#j:state
+                if (seqNum ==B_col-1):
+                    beta[j][seqNum] = 0;
+                else:#beta[j][i]=sigma(k):ajk*B[k,i+1]*beta[k][i+1]
+                    p = -1*10**20
+                    for k in xrange(B_row):
+                        p= self.addLog(p,(beta[k][seqNum+1]+self.getLog(self.A[j,k])+self.getLog(B[k,seqNum+1])))
+                    beta[j][seqNum]=p
+        return beta
+    def EM_learn(self,B):
+        B_row,B_col=B.shape
+        apha=self.calc_Log_apha(B)
+        beta=self.calc_Log_beta(B)
+        #print "apha:",apha
+        #print "beta:",beta
+        gamma=[]#pass through state i
+        eta=[]#pass from i to j,eta[i][j][time],from state i to state j
+        for i in xrange(B_row):
+            gamma.append([])
+            eta.append([])
+            for j in xrange(B_row):
+                eta[i].append([])
+        #end initialize
+        #calc gamma
+        pg=-1*10**20
+        for i in xrange(B_col):
+            for j in xrange(B_row):
+                pg=self.addLog(pg,apha[j][i]+beta[j][i])
+            for j in xrange(B_row):
+                gamma[j].append(apha[j][i]+beta[j][i]-pg)
+        #calc eta
+        pe=-1*10**20
+        for i in xrange(B_col-1):
+            for j in xrange(B_row):
+                for k in xrange(B_row):
+                    pe=self.addLog(pe,apha[j][i]+self.getLog(self.A[j,k]*B[k,i+1])+beta[k][i+1])
+            for j in xrange(B_row):
+                for k in xrange(B_row):
+                    eta[j][k].append(apha[j][i]+self.getLog(self.A[j,k]*B[k,i+1])+beta[k][i+1]-pe)
+            
+        for i in xrange(B_row):
+            for j in xrange(B_row):
+                storeGamma=-1*10**20
+                storeEta=-1*10**20
+                for k in xrange(B_col-1):
+                    storeGamma=self.addLog(storeGamma,gamma[i][k])
+                    storeEta=self.addLog(storeEta,eta[i][j][k])
+                self.A[i,j]=10**(storeEta-storeGamma)
+        '''
+        for i in xrange(B_row):
+            print self.A[i,:]
+            count=0
+            for j in xrange(B_row):
+                count+=self.A[i,j]
+            print "count:",count
+        '''
+    def addLog(self,x,y):#from logX,logY to log(X+Y)
         
+        if x<y:
+            temp=y
+            y=x
+            x=temp
+        diff=y-x
+        if diff<(-9):
+            return x
+        else:
+            z=10**diff
+            return x+self.getLog(1+z)
+
+    
     def Viterbi(self,B):#pi for initial distribution;A for transition matrix A(i,j):state i to state j; B for conditional probability:B[i,j]=P(jth Output|state i);O output observe sequence
         
         B_row,B_col=B.shape
@@ -107,7 +142,7 @@ class HMM_core(object):
             #print "max:",self.getMax(value)
         #find max path over, then trace back
         maxValue,maxState=self.getMax(value)
-        print "out"
+        #print "out"
         #print path
         y_sequence=[]
         for i in xrange(B_col):#make y_sequence are [B_col][]
@@ -117,7 +152,7 @@ class HMM_core(object):
             maxState=path[maxState][B_col-i-2]
         return y_sequence
     def getMax(self,array):
-        maximum=-10000000
+        maximum=-1*(10**20)
         maxState=0
         for i in xrange(len(array)):
             if array[i]>maximum:
@@ -126,6 +161,6 @@ class HMM_core(object):
         return maximum,maxState
     def getLog(self,Num):
         if Num==0:
-            return -100000
+            return -1*(10**19)
         else:
             return math.log(Num,10)
