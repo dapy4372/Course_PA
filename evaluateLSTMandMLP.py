8554e2adc74ea6916b3e7e2374b57d16eac786e0
douduck08 <lu10082000@gmail.com>
2016-01-05 00:59:53 +0800

    add feature for LSTM

--- evaluateLSTMandMLP.py
+++ evaluateLSTMandMLP.py
@@ -1,26 +1,77 @@
-import pdb
 import sys
 import csv
 import string
+import numpy as np
 import argparse
 
-#from progressbar import Bar, ETA, Percentage, ProgressBar
 from keras.models import model_from_json
 from spacy.en import English
-import numpy as np
 import scipy.io
-from sklearn.externals import joblib
 
 sys.path.insert(0,'../data/')
 # from preprocess import get_answers
 sys.path.insert(0,'../scripts')
 
-from itertools import izip_longest
-def grouper(iterable, n, fillvalue=None):
-    args = [iter(iterable)] * n
-    return izip_longest(*args, fillvalue=fillvalue)
-
 # python evaluateLSTMandMLP.py -model model/2016010401_LSTM_default_model.json -weights model/2016010401_LSTM_default_model_epock_025.hdf5 -choices /share/MLDS/cluster_results/choices_kmeans_test_1000.txt -results 2016010401_testing_result.txt
+
+img_dim = 4096
+def getImageFeature(imageData, idList):
+    batchSize = len(idList)
+    imageMatrix = np.zeros((batchSize, img_dim), dtype = 'float32')
+    for i in xrange(batchSize):
+        imageMatrix[i,:] = imageData[ idList[i] ]
+    return imageMatrix
+
+word_vec_dim = 300
+def getQuestionWordVector(questionData, idList, wordVectorModel):
+    batchSize = len(idList)
+    maxlen = 0
+    tokens = []
+    for i in xrange(batchSize):
+        tokens.append( wordVectorModel( questionData[ idList[i] ].decode('utf8')) )
+        maxlen = max(maxlen, len(tokens[i]))
+    questionMatrix = np.zeros((batchSize, maxlen, word_vec_dim), dtype = 'float32')
+    for i in xrange(batchSize):
+        for j in xrange(len(tokens[i])):
+            questionMatrix[i,j,:] = tokens[i][j].vector
+    return questionMatrix
+
+def loadData():
+    idMap = {}
+    with open('/share/MLDS/preprocessed/id_test.txt', 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter = ' ')
+        for row in reader:
+            idMap[int(row[1])] = int(row[0])
+
+    questionIdList = idMap.keys()
+    questionData = {}
+    with open('/share/MLDS/preprocessed/questions_test.txt', 'r') as txtfile:
+        questionsTrain = txtfile.read().splitlines()
+        if (len(questionIdList) != len(questionsTrain)):
+            print "*** load question error ***"
+        for i in xrange(len(questionIdList)):
+            questionData[questionIdList[i]] = questionsTrain[i]
+
+    imageData = {}
+    with open('/share/MLDS/final_img_feat.txt', 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter = ' ')
+        for row in reader:
+            imageData[int(row[0])] = np.array(row[1:]).astype(dtype = 'float32')
+
+    return idMap, questionData, imageData
+
+def prepareIdList(idList, batchSize):
+    questionNum = len(idList)
+    batchNum = questionNum / batchSize
+    # random.shuffle(idList)
+    idListInBatch = []
+    for i in xrange(batchNum):
+        idListInBatch.append( idList[i * batchSize : (i+1) * batchSize] )
+    if questionNum % batchSize != 0:
+        idListInBatch.append( idList[batchNum * batchSize :] )
+        batchNum += 1
+    return idListInBatch, batchNum
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-model', type=str, required=True)
@@ -37,10 +88,7 @@ def main():
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
     origin_data_path = '/share/MLDS/'
-    if args.predict_type == 'train':
-        questions_id_filename = origin_data_path + 'preprocessed/id_train.txt'
-        questions = open(origin_data_path + 'preprocessed/questions_train.txt', 'r').read().decode('utf8').splitlines()
-    elif args.predict_type == 'test':
+    if args.predict_type == 'test':
         questions_id_filename = origin_data_path + 'preprocessed/id_test.txt'
         questions = open(origin_data_path + 'preprocessed/questions_test.txt', 'r').read().decode('utf8').splitlines()
     else:
@@ -50,24 +98,27 @@ def main():
     with open(questions_id_filename, "r") as infile:
         reader = csv.reader(infile, delimiter = ' ')
         for row in reader:
-            questions_id.append(row[1])
+            questions_id.append(int(row[1]))
 
     nlp = English()
-    print 'loaded word2vec features'
 
-    nb_classes = 3000
+    print '*** load data ***'
+    # idMap, questionData, imageData, answerData = testData()
+    idMap, questionData, imageData = loadData()
+    print idMap.items()[0]
+    print questionData.items()[0]
+    print imageData.items()[0]
+
     y_predict = []
     batchSize = 128
-    #widgets = ['Evaluating ', Percentage(), ' ', Bar(marker='#',left='[',right=']'), ' ', ETA()]
-    #pbar = ProgressBar(widgets=widgets)
+    questionIdList, batchNum = prepareIdList(idMap.keys(), batchSize)
 
-    for qu_batch in grouper(questions, batchSize, fillvalue=questions[0]):
-        X_q_batch = get_questions_matrix_sum(qu_batch, nlp)
-        X_batch = X_q_batch
-        y_predict.extend(model.predict_classes(X_batch, verbose=0))
-        #y_predict_text.extend(labelencoder.inverse_transform(y_predict))
+    for j in xrange(batchNum):
+        imageIdListForBatch = [idMap[key] for key in questionIdList[j]]
+        y_predict.extend(model.predict_classes([ getImageFeature(imageData, imageIdListForBatch),
+                                                 getQuestionWordVector(questionData, questionIdList[j], nlp) ],
+                                               verbose=0))
 
-    #pdb.set_trace()
     # choose choice by cluster number
     answers_predict = []
     with open(args.choices, 'r') as infile:
@@ -80,20 +131,6 @@ def main():
             else:
                 answers_predict.append(0)
             cur_choice += 1
-
-    # check train correct rate
-    if args.predict_type == 'train':
-        # answers = get_answers(args.answers)
-        nb_answers = answers.shape[0]
-        results = np.all([answers, np.array(answers_predict, dtype = 'int32')], axis = 0)
-        nb_correct = np.sum(results)
-        correct_rate = float(nb_correct) / float(nb_answers)
-        modelparams_str = (args.model.split('/')[-1]).split('.')[0] + '\n'
-        eval_str = str.format("{0:.2f}", correct_rate*100) + ' ' + str(nb_answers) + ' ' + str(nb_correct) + '\n'
-        with open('../log/train_correct_rate.log', "a") as outfile:
-            outfile.write(modelparams_str)
-            outfile.write(eval_str)
-        print eval_str
 
     # write testing answer to file which will be uploaded
     if args.predict_type == 'test':
