dc875e277d4e302b5c3f44f40e5e9a63b03c6bcd
douduck08 <lu10082000@gmail.com>
2016-01-18 12:04:18 +0800

    fix bug

--- customidf.py
+++ customidf.py
@@ -6,6 +6,7 @@ import argparse
 
 def parseArgs():
     parser = argparse.ArgumentParser()
+    parser.add_argument('-custom', type=bool, default=True)
     parser.add_argument('-memory_limit', type=float, default=6.0)
     return parser.parse_args()
 
@@ -16,10 +17,13 @@ def limit_memory(maxsize):
 customLevel = 300
 customList = ['how', 'what', 'why', 'where', 'who', 'which', 'many', 'color', 'number', 'time']
 removeList = ['?', '\'s', ',', '(', ')', ':', '\\', '\'']
-def splitQuestion(question):
+def splitQuestion(question, remove_duplicate = True):
     for pattern in removeList:
         question = question.replace(pattern,'')
-    return list(set(question.split(' ')))
+    if remove_duplicate:
+        return list(set(question.split(' ')))
+    else:
+        return question.split(' ')
 
 if __name__ == "__main__":
     arg = parseArgs()
@@ -56,8 +60,12 @@ if __name__ == "__main__":
                     wordMap[word] = 1
 
     print '*** custom the important words ***'
-    for word in customList:
-        wordMap[word] /= customLevel
+    if arg.custom:
+        filename = 'question_custom_idf'
+        for word in customList:
+            wordMap[word] /= customLevel
+    else:
+        filename = 'question_standard_idf'
 
     print '*** calculate the idf of words ***'
     words = wordMap.keys()
@@ -71,20 +79,20 @@ if __name__ == "__main__":
     print '*** build the question weight file ***'
     with open('data/processed_text/question_processed.train', 'r') as questionfile:
         reader = csv.reader(questionfile, delimiter = '\t')
-        with open('data/processed_text/question_custom_idf.train', 'w') as outfile:
+        with open('data/processed_text/' + filename + '.train', 'w') as outfile:
             writer = csv.writer(outfile, delimiter = ' ')
             for question in reader:
-                word = splitQuestion(question[2])
+                word = splitQuestion(question[2], True)
                 weights = np.zeros(len(words), dtype = 'float32')
                 for i in xrange(len(words)):
                     weights[i] = wordMap[words[i]]
                 writer.writerow([question[0], question[1]] + weights.tolist())
     with open('data/processed_text/question_processed.test', 'r') as questionfile:
         reader = csv.reader(questionfile, delimiter = '\t')
-        with open('data/processed_text/question_custom_idf.test', 'w') as outfile:
+        with open('data/processed_text/' + filename + '.test', 'w') as outfile:
             writer = csv.writer(outfile, delimiter = ' ')
             for question in reader:
-                word = splitQuestion(question[2])
+                word = splitQuestion(question[2], True)
                 weights = np.zeros(len(words), dtype = 'float32')
                 for i in xrange(len(words)):
                     weights[i] = wordMap[words[i]]
