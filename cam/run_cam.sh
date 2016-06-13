67d5b793dfeb86b4f7a9e33d3ccce4f47f49a414
Roy <b01501061@ntu.edu.tw>
2016-06-13 22:02:56 +0000

    DIR for remote access

--- cam/run_cam.sh
+++ cam/run_cam.sh
@@ -1,8 +1,8 @@
-video_dir=./data/video
+#! /bin/bash
 
-for i in `seq 1 3` 
+for i in `seq 1 10` 
 do
-    video_name=${video_dir}/$(date +"%Y%m%d%H%M%S").mp4
+    video_name=${pi_video_dir}/$(date +"%Y%m%d%H%M%S").mp4
     raspivid -o - -t $video_time -w 320 -h 320 -hf -fps 15 | ffmpeg -r 15 -i - -vcodec copy -f mp4 $video_name
     echo $video_name
 done
--- cam/run_cap.sh
+++ cam/run_cap.sh
@@ -1,7 +1,8 @@
+#! /bin/bash
 while read line
 do
-    python cap.py $line $video_time
-    ./sync.sh
+    python $DIR/cap.py $line $video_time
+    $DIR/sync.sh
     rm -f $pi_video_dir/$line.mp4
     rm -f $pi_image_dir/$line*.png
     rm -f $pi_table_dir/$line*.txt
