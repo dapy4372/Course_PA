58bd22520d9e23526a180ec0ee5b0108850af3b3
Roy <b01501061@ntu.edu.tw>
2016-06-13 21:16:19 +0000

    filename

--- cam/run_cam.sh
+++ cam/run_cam.sh
@@ -2,7 +2,7 @@ video_dir=./data/video
 
 for i in `seq 1 3` 
 do
-    video_name=${video_dir}/video_$(date +"%Y%m%d%H%M%S").mp4
+    video_name=${video_dir}/$(date +"%Y%m%d%H%M%S").mp4
     raspivid -o - -t $video_time -w 320 -h 320 -hf -fps 15 | ffmpeg -r 15 -i - -vcodec copy -f mp4 $video_name
     echo $video_name
 done
