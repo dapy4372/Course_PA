7d8358473791f852b02325240d46083057e29ef6
user <smile_sunday@hotmail.com>
2016-06-14 06:00:35 +0800

    fork

--- new_ui/main.cpp
+++ new_ui/main.cpp
@@ -1,9 +1,16 @@
 #include <QApplication>
+#include <unistd.h>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
+    int pid = fork();
+    if( pid == 0 ) {
+        system("ssh pi@192.168.1.132 '/home/pi/Roy/es_finalproject/cam/run.sh'");
+        exit(0);
+    }
     QApplication app(argc, argv);
     MainWindow mainwindow;
     return app.exec();
 }
+
