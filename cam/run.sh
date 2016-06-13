3054618a0508acb8fb445e6716198fce72cbd808
Roy <b01501061@ntu.edu.tw>
2016-06-13 20:30:55 +0000

    export variable and check dir exist

--- cam/run.sh
+++ cam/run.sh
@@ -1,2 +1,24 @@
-export video_time=3000
+export video_time=10000
+
+export pi_data_dir=/home/pi/data
+export pc_data_dir=/home/user/data
+
+export pi_video_dir=$pi_data_dir/video/
+export pi_image_dir=$pi_data_dir/image/
+export pi_table_dir=$pi_data_dir/table/
+
+export pc_video_dir=$pc_data_dir/video
+export pc_image_dir=$pc_data_dir/image
+export pc_table_dir=$pc_data_dir/table
+
+export pc=user@192.168.1.190
+
+[ -d $pi_video_dir ] || mkdir $pi_video_dir
+[ -d $pi_image_dir ] || mkdir $pi_image_dir
+[ -d $pi_table_dir ] || mkdir $pi_table_dir
+
+ssh pc '[ -d $pc_video_dir ] || mkdir $pc_video_dir'
+ssh pc '[ -d $pc_image_dir ] || mkdir $pc_image_dir'
+ssh pc '[ -d $pc_table_dir ] || mkdir $pc_table_dir'
+
 ./run_cam.sh | ./run_cap.sh
--- cam/sync.sh
+++ cam/sync.sh
@@ -1,18 +1,5 @@
 #! /bin/bash
 
-pi_data_dir=/home/pi/data
-pc_data_dir=/home/user/data
-
-pi_video_dir=$pi_data_dir/video/
-pi_image_dir=$pi_data_dir/image/
-pi_table_dir=$pi_data_dir/image/
-
-pc_video_dir=$pc_data_dir/video
-pc_image_dir=$pc_data_dir/image
-pc_table_dir=$pc_data_dir/image
-
-pc=user@192.168.1.190
-
 rsync -a --ignore-existing -e "ssh" $pi_video_dir $pc:$pc_video_dir
 rsync -a --ignore-existing -e "ssh" $pi_image_dir $pc:$pc_image_dir 
 rsync -a --ignore-existing -e "ssh" $pi_table_dir $pc:$pc_table_dir
