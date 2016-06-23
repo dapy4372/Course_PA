e7570a8664bb3c7820815a0e4aae2abe5c33550f
Roy <b01501061@ntu.edu.tw>
2016-06-23 14:13:01 +0000

    edit run_cap.sh

--- cam/run_cap.sh
+++ cam/run_cap.sh
@@ -2,7 +2,7 @@
 while read line
 do
     $DIR/cap.py $pi_video_dir/$line.mp4 $video_time
-    $DIR/grp.py $pi_image_dir $pi_data_dir/grp_list
+    $DIR/grp.py $pi_image_dir $pi_data_dir/grp_list $pi_data_dir/img_faceId_map
     $DIR/sync.sh
     rm -f $pi_video_dir/$line.mp4
     rm -f $pi_image_dir/$line*.jpg
